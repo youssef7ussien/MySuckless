88ea8ed982d3c78fbd952d3b2b784ed0c735560f
youssef7ussien <youssef7ussien@gmail.com>
2023-12-09 16:09:08 +0200

    [dwm] Change Theme

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,17 +6,17 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "JetBrainsMono NF:size=9" };
+static const char dmenufont[]       = "JetBrainsMono NFM:size=9";
+static const char col_gray1[]       = "#1D1D1D";
+static const char col_gray2[]       = "#665c54";
+static const char col_gray3[]       = "#d5c4a1";
+static const char col_gray4[]       = "#ebdbb2";
+static const char col_gray5[]        = "#32302f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+  [SchemeNorm] = { col_gray3, col_gray1, col_gray5 },
+	[SchemeSel]  = { col_gray4, col_gray5,  col_gray2  },
 };
 
 /* tagging */
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
