249422f6c1501a70c8a286277a82cbf18879c488
youssef7ussien <youssef7ussien@gmail.com>
2023-12-30 20:57:50 +0200

    [dmenu] Change theme

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 8;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = 4;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -11,8 +11,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#d5c4a1", "#1D1D1D" },
+	[SchemeSel] = { "#ebdbb2", "#32302f" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
