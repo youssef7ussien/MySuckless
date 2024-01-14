7115602d9ccd6bc686ff89e4bf7471d22716ecee
youssef7ussien <youssef7ussien@gmail.com>
2024-01-14 04:12:00 +0200

    [dwm] Adding some new rules in config.def.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -31,11 +31,18 @@ static const Rule rules[] = {
 	 */
 	/* class                  instance    title           tags mask    iscentered     switchtag    isfloating   monitor */
 	{ "Gimp",                 NULL,       NULL,           0,           0,             1,           1,           -1 },
-	{ "firefox",              NULL,       NULL,           1 << 8,      0,             1,           0,           -1 },
-	{ "Google-chrome",        NULL,       NULL,           1,           0,             1,           0,            1 },
-  { "st-256color",          NULL,       "htop",         0,           1,             0,           1,           -1 },
-  { "mpv",                  NULL,       NULL,           0,           1,             0,           1,           -1 },
   { "jetbrains-studio",     NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "VSCodium",             NULL,       NULL,           1 << 1,      0,             0,           0,            0 },
+  { "firefox",              NULL,       NULL,           1 << 8,      0,             1,           0,           -1 },
+	{ "Google-chrome",        NULL,       NULL,           1,           0,             1,           0,            1 },
+  { "chromium-float",       NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "firefox-float",        NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "st-256color-float",    NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "tmux-256color-float",  NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "mpv",                  NULL,       NULL,           0,           1,             0,           1,           -1 },
+  { "ticktick",             NULL,       NULL,           1 << 7,      1,             0,           1,           -1 },
+  { "TelegramDesktop",      NULL,       NULL,           1 << 7,      1,             1,           1,           -1 },
+  { "Qalculate-gtk",        NULL,       NULL,           0,           1,             0,           1,           -1 },
 };
 
 /* layout(s) */
@@ -67,7 +74,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+// static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
