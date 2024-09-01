ad2feb67a5b9f9145ebd797eb9c89577b5ed1b8f
youssef7ussien <youssef7ussien@gmail.com>
2024-09-01 06:50:45 +0300

    [dmenu] feat: Add more fonts.

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const int user_bh = 4;               /* add an defined amount of pixels to the bar height */
 
-static const char *fonts[] = { "JetBrainsMono NF:size=9" };
+static const char *fonts[] = { "JetBrainsMono NF:size=9", "DejaVu Sans:size=9", "NotoColorEmoji:pixelsize=9:antialias=true:autohint=true" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
